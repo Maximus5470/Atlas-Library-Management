5cb3f993adcc15fad1b2e24e86649f583997fcf8
Maximus5470 <gauthamsharma2005@gmail.com>
2025-04-20 09:51:49 +0530

    implementing emailjs and workflows

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,6 +1,27 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "@/lib/config";
+import emailjs from "@emailjs/browser";
+
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+export const sendEmail = async (
+  subject: string,
+  email: string,
+  name: string,
+  message: string,
+) => {
+  await emailjs.send(
+    config.env.emailjs.serviceId,
+    config.env.emailjs.templateId,
+    {
+      subject: subject,
+      name: name,
+      email: email,
+      message: message,
+    },
+    config.env.emailjs.publicKey,
+  );
+};
